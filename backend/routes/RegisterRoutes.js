be77689896091db16c395cc345782b099c815706
sowjanya4034 <140581675+sowjanya4034@users.noreply.github.com>
2024-11-06 20:40:48 +0530

    Update RegisterRoutes.js

--- backend/routes/RegisterRoutes.js
+++ backend/routes/RegisterRoutes.js
@@ -1,1 +1,9 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const RegisterModel=require("../models/RegisterModel");
+const router=express.Router();
+router.post("/save",(req,res)=>{
+    res.send("Hello from Register save");
+});
 
+module.exports=router;
